d41c919b662d6977158d73fd5ada4edf7b475261
Hariom Vashista <rishivashista4@gmail.com>
2026-05-06 08:47:10 +0530

    add selection functionality in product import

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router";
-import styles from "../styles/index.css";
+import "../styles/index.css";
 
 export default function Index() {
   const navigate = useNavigate();
   return (
     <s-page heading="Meta Editor">
       <div className="resource-container">
-        <button className="resource-item" onClick={() => navigate("/app/products")}>
+        <button className="resource-item" onClick={() => {navigate("/app/products")}}>
           <s-box >
             <img src="/products.png" width={250} height={200} />
             <h3 className="resource-name">Products</h3>
@@ -21,8 +21,4 @@ export default function Index() {
       </div>
     </s-page>
   );
-}
-
-export function links() {
-  return [{ rel: "stylesheet", href: styles }];
 }
--- app/routes/app.products.export.jsx
+++ app/routes/app.products.export.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData, useNavigate } from "react-router";
 import "../styles/product-export.css";
+import { useEffect, useState } from "react";
 
 export const loader = async ({ request }) => {
   const { authenticate } = await import("../shopify.server.js");
@@ -61,10 +62,56 @@ export const loader = async ({ request }) => {
   return json;
 };
 
+export const action = async ({ request }) => {
+  const { authenticate } = await import("../shopify.server.js");
+  const { admin } = await authenticate.admin(request);
+
+  const body = await request.json();
+  const { ids } = body;
+
+  const response = await admin.graphql(
+    `
+      query GetProductByIds($ids: [ID!]!) {
+        nodes(ids: $ids) {
+          ... on  Product {
+            id
+            handle
+            title
+            metafields(first: 50) {
+              edges {
+                node {
+                  namespace
+                  key
+                  type
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: {
+        ids: ids || null,
+      },
+    },
+  );
+  const json = await response.json();
+  console.log(json);
+  return null;
+};
+
 function ProductExport() {
   const data = useLoaderData();
   const navigate = useNavigate();
-  console.log(data);
+
+  const [selectedProducts, setSelectedProducts] = useState(new Set());
+  const pageIds = data.data.products.nodes.map((node) => {
+    return node.id;
+  });
+  const isAllSelected =
+    pageIds.length > 0 && pageIds.every((id) => selectedProducts.has(id));
 
   const handleNext = () => {
     const cursor = data.data.products.pageInfo.endCursor;
@@ -76,13 +123,59 @@ function ProductExport() {
     navigate(`?cursor=${cursor}&direction=prev`);
   };
 
+  const handleSelection = (id) => {
+    setSelectedProducts((prev) => {
+      const newSet = new Set(prev);
+
+      if (newSet.has(id)) {
+        newSet.delete(id);
+      } else {
+        newSet.add(id);
+      }
+      return newSet;
+    });
+  };
+  const handleAllSelection = (ids) => {
+    setSelectedProducts((prev) => {
+      const newSet = new Set(prev);
+      const allSelected = ids.every((id) => newSet.has(id));
+
+      if (allSelected) {
+        ids.forEach((id) => newSet.delete(id));
+      } else {
+        ids.forEach((id) => newSet.add(id));
+      }
+
+      return newSet;
+    });
+  };
+
+  const exportSelected = async ()=>{
+    const ids = Array.from(selectedProducts);
+
+    await fetch("/app/products/export", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ids})
+    })
+  }
   return (
     <div>
       <s-section>
-        <div className="product-status">
-          <s-clickable>All</s-clickable>
-          <s-clickable>Active</s-clickable>
-          <s-clickable>Draft</s-clickable>
+        <div className="product-container">
+          <div className="product-status-container">
+            <s-clickable>All</s-clickable>
+            <s-clickable>Active</s-clickable>
+            <s-clickable>Draft</s-clickable>
+          </div>
+          <div className="export-buttons">
+            <s-button>Export all products</s-button>
+            {selectedProducts.size > 0 ? (
+              <s-button onClick={exportSelected}>Export {selectedProducts.size} products</s-button>
+            ) : null}
+          </div>
         </div>
         <s-table>
           <s-table-header-row>
@@ -90,6 +183,13 @@ function ProductExport() {
             <s-table-header>Name</s-table-header>
             <s-table-header>Status</s-table-header>
             <s-table-header>Number of Metafields</s-table-header>
+            <s-table-header>
+              <input
+                type="checkbox"
+                checked={isAllSelected}
+                onChange={() => handleAllSelection(pageIds)}
+              />
+            </s-table-header>
           </s-table-header-row>
           <s-table-body>
             {data.data.products.nodes.map((node) => {
@@ -111,6 +211,13 @@ function ProductExport() {
                         ? "5+"
                         : node.metafields.nodes.length}{" "}
                     metafields
+                  </s-table-cell>
+                  <s-table-cell>
+                    <input
+                      type="checkbox"
+                      checked={selectedProducts.has(node.id)}
+                      onChange={() => handleSelection(node.id)}
+                    />
                   </s-table-cell>
                 </s-table-row>
               );
@@ -135,7 +242,6 @@ function ProductExport() {
           </button>
         </div>
       </s-section>
-      <s-button>Export metafield</s-button>
     </div>
   );
 }
